62c0f79ee090bf49299650790c29125316049556
Nabheet Madan <Nabheet.cloud@gmail.com>
2018-04-18 17:23:34 +0530

    Model Relationships exercise 10

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,6 +17,30 @@
                            
                                 <a href="/posts/create" class="btn btn-primary">Create Post</a>
                                 <h3> Your Blog posts</h3>
+                                @if(count($posts)>0)
+                                <table class="table table-striped">
+                                    <tr>
+                                        <th>Title</th>
+                                        <th>Body</th>
+                                        <th></th>
+                                    </tr>
+                                    @foreach($posts as $post)
+                                    <tr>
+                                    <td>{{$post->title}} by {{$post->user->name}}</td>
+                                    <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default" >Edit</a></td>
+                                    <td>
+                                            {!!Form::open(['action' =>['PostsContoller@destroy', $post->id],"method"=>"POST","class"=>"pull-right"])!!}
+                                            {{Form::hidden('_method','DELETE')}}
+                                            {{Form::submit('Delete',["class"=>"btn btn-danger"])}}
+                                        {!!Form::close()!!}
+
+                                    </td>
+                                        </tr>
+                                    @endforeach
+                                </table>   
+                                @else
+                                <p>You have no Posts</p>
+                                @endif
                         </div>
                 </div>
             </div>
